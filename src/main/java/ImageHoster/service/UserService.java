8a8d3bc48772913575ee62d3c43ca259e045b7c0
Iftikar Khan <ik@sparxinnovations.com>
2020-10-25 13:45:12 +0530

    Bug fixed, and feature implemented

---
+++ src/main/java/ImageHoster/service/UserService.java
@@ -0,0 +1,58 @@
+package ImageHoster.service;
+
+import ImageHoster.model.User;
+import ImageHoster.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //Call the registerUser() method in the UserRepository class to persist the user record in the database
+    public void registerUser(User newUser) {
+        userRepository.registerUser(newUser);
+    }
+
+    //Since we did not have any user in the database, therefore the user with username 'upgrad' and password 'password' was hard-coded
+    //This method returned true if the username was 'upgrad' and password is 'password'
+    //But now let us change the implementation of this method
+    //This method receives the User type object
+    //Calls the checkUser() method in the Repository passing the username and password which checks the username and password in the database
+    //The Repository returns User type object if user with entered username and password exists in the database
+    //Else returns null
+    public User login(User user) {
+        User existingUser = userRepository.checkUser(user.getUsername(), user.getPassword());
+        if (existingUser != null) {
+            return existingUser;
+        } else {
+            return null;
+        }
+    }
+
+    public boolean isPasswordValid(String password){
+        String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&-+=()])(?=\\S+$).{4}$";
+        // Compile the ReGex
+        Pattern p = Pattern.compile(regex);
+        // If the password is empty
+        // return false
+        if (password == null) {
+            return false;
+        }
+
+        // Pattern class contains matcher() method
+        // to find matching between given password
+        // and regular expression.
+        Matcher m = p.matcher(password);
+
+        // Return if the password
+        // matched the ReGex
+        return m.matches();
+    }
+
+}
